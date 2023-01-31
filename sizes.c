f097ccc6e08e06b04c8351af986fd2cdefb439c1
Cooper Anderson <canderson@gocva.com>
2023-01-31 12:13:16 -0500

    i dont know anymore

---
+++ sizes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("unsigned char     %d bytes(s)\n", sizeof(unsigned char));
+    printf("signed char     %d bytes(s)\n", sizeof(signed char));
+    printf("signed short     %d bytes(s)\n", sizeof(signed short));
+    printf("int     %d bytes(s)\n", sizeof(int));
+    printf("long     %d bytes(s)\n", sizeof(long));
+    printf("long long     %d bytes(s)\n", sizeof(long long));
+    printf("float     %d bytes(s)\n", sizeof(float));
+    printf("double     %d bytes(s)\n", sizeof(double));
+
+
+}
+
